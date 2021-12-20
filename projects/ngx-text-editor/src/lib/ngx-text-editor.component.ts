7d3d9f17bd613555635cf59071023c60fd18028c
Matheus Cruz de Souza <matheuscruzsouza@gmail.com>
2021-12-20 00:52:56 -0300

    Update

--- projects/ngx-text-editor/src/lib/ngx-text-editor.component.ts
+++ projects/ngx-text-editor/src/lib/ngx-text-editor.component.ts
@@ -15,71 +15,68 @@ export class NgxTextEditorComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     const buttons = document.querySelectorAll('button');
-    const textField = document.getElementById('textField') as HTMLIFrameElement;
+    const iframe = document.getElementById('textField') as HTMLIFrameElement;
+    const content = iframe.contentDocument || iframe.contentWindow?.document;
 
     let show = false;
 
-    if (textField.contentDocument) {
-      textField.contentDocument.designMode = 'On';
-    }
-    
-    buttons.forEach(button => {
-      button.addEventListener('click', () => {
-        let cmd = button.getAttribute('data-cmd') || '';
+    if (content) {
+      content.designMode = 'On';
 
-        if (button.name === 'active') {
-          button.classList.toggle('active');
-        }
-
-        if (['insertImage', 'createLink'].includes(cmd) && textField) {
-          let url = prompt('Insira o link aqui', '')  || '';
-          textField.contentDocument?.execCommand(cmd, false, url);
-
-          if (cmd === 'insertImage') {
-            const images = textField.contentDocument?.querySelectorAll('img') || [];
-
-            images.forEach((image: HTMLImageElement) => {
-              image.style.width = '100%';
-            });
-            
-          } else {
-            const links = textField.contentDocument?.querySelectorAll('a') || [];
-
-            links.forEach((link: HTMLAnchorElement) => {
-              link.target = '_blank';
-
-              link.addEventListener('mouseover', () => {
-                if (textField.contentDocument) {
-                  textField.contentDocument.designMode = 'Off';
-                } 
-              });
-              link.addEventListener('mouseout', () => {
-                if (textField.contentDocument) {
-                  textField.contentDocument.designMode = 'On';
-                } 
-              });
-            });
-
+      buttons.forEach(button => {
+        button.addEventListener('click', () => {
+          let cmd = button.getAttribute('data-cmd') || '';
+  
+          if (button.name === 'active') {
+            button.classList.toggle('active');
           }
-        } else {
-          textField.contentDocument?.execCommand(cmd, false, undefined);
-        }
-
-        if (cmd === 'showCode') {
-          const textBody = textField.contentDocument?.querySelector('body');
-
-          if (textBody) {
-            if (show) {
-              textBody.innerHTML = textBody.textContent || '';
-              show = false;
+  
+          if (['insertImage', 'createLink'].includes(cmd)) {
+            let url = prompt('Insira o link aqui', '')  || '';
+            content.execCommand(cmd, false, url);
+  
+            if (cmd === 'insertImage') {
+              const images = content.querySelectorAll('img') || [];
+  
+              images.forEach((image: HTMLImageElement) => {
+                image.style.width = '100%';
+              });
+              
             } else {
-              textBody.textContent = textBody.innerHTML;
-              show = true;
+              const links = content.querySelectorAll('a') || [];
+  
+              links.forEach((link: HTMLAnchorElement) => {
+                link.target = '_blank';
+  
+                link.addEventListener('mouseover', () => {
+                  content.designMode = 'Off';
+                });
+                link.addEventListener('mouseout', () => {
+                  content.designMode = 'On';
+                });
+              });
+  
+            }
+          } else {
+            content.execCommand(cmd, false, undefined);
+          }
+  
+          if (cmd === 'showCode') {
+            const textBody = content.querySelector('body');
+  
+            if (textBody) {
+              if (show) {
+                textBody.innerHTML = textBody.textContent || '';
+                show = false;
+              } else {
+                textBody.textContent = textBody.innerHTML;
+                show = true;
+              }
             }
           }
-        }
+        });
       });
-    });
+    }
   }
 
   onKey(event: any): void {
