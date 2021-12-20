aac02a97f568f8ac8efdb7aaad3baac3f1ebace4
Matheus Cruz de Souza <matheuscruzsouza@gmail.com>
2021-12-20 16:20:53 -0300

    Set ngFor

--- projects/ngx-text-editor/src/lib/ngx-text-editor.component.ts
+++ projects/ngx-text-editor/src/lib/ngx-text-editor.component.ts
@@ -12,7 +12,7 @@ export class NgxTextEditorComponent implements AfterViewInit {
 
   editable = true;
 
-  selectedColor = new FormControl("#000");
+  selectedColor = "#000";
 
   fonts = [
     'Arial',
