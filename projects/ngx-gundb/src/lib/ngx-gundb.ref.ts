56572bac1b9c7b6a6b322608ec59cb66a3427a6a
Matheus Cruz de Souza <matheuscruzsouza@gmail.com>
2022-01-17 18:27:23 -0300

    Fix new function

--- projects/ngx-gundb/src/lib/ngx-gundb.ref.ts
+++ projects/ngx-gundb/src/lib/ngx-gundb.ref.ts
@@ -221,7 +221,7 @@ export class NgxGundbRef {
       const [key, value] = entrie;
 
       if (value['#']) {
-        this.gun.get(value).val().subscribe((_data: any) => cache[key] = this.buildTree(_data));
+        this.get(value).once().subscribe((_data: any) => cache[key] = this.buildTree(_data));
       } else if (typeof value === 'object') {
         cache[key] = this.buildTree(value);
       } else {
