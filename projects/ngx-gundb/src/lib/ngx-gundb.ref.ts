1320e2a6c42cf75212872f9a7bb9895bcab5a418
Matheus Cruz de Souza <matheuscruzsouza@gmail.com>
2022-01-17 21:25:07 -0300

    Add new logs

--- projects/ngx-gundb/src/lib/ngx-gundb.ref.ts
+++ projects/ngx-gundb/src/lib/ngx-gundb.ref.ts
@@ -220,7 +220,11 @@ export class NgxGundbRef {
     Object.entries(data).forEach((entrie: [string, any]) => {
       const [key, value] = entrie;
 
+      console.log("VALUE: ", value);
+
+
       if (value['#']) {
+        this.get(value['#']).val().subscribe((_data: any) => console.log("value['#']: ", _data));
         this.get(value['#']).val().subscribe((_data: any) => cache[key] = _data);
       } else if (typeof value === 'object') {
         cache[key] = this.buildTree(value);
