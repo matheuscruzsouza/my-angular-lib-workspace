96d8d2a68d976578010d669adb911d6f1db9d7cc
Matheus Cruz de Souza <matheuscruzsouza@gmail.com>
2022-01-17 21:13:09 -0300

    Update the new feature

--- projects/ngx-gundb/src/lib/ngx-gundb.ref.ts
+++ projects/ngx-gundb/src/lib/ngx-gundb.ref.ts
@@ -221,7 +221,7 @@ export class NgxGundbRef {
       const [key, value] = entrie;
 
       if (value['#']) {
-        this.get(value).val().subscribe((_data: any) => cache[key] = _data);
+        this.gun.get(value).val().subscribe((_data: any) => cache[key] = _data);
       } else if (typeof value === 'object') {
         cache[key] = this.buildTree(value);
       } else {
